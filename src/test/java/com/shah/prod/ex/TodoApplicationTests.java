f48cef357a872588527dd9d098e1aa694af48d0e
SHAH <s.shamuratov@p-s.kz>
2022-11-12 20:09:15 +0600

    first commit

---
+++ src/test/java/com/shah/prod/ex/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shah.prod.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
